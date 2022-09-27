b026fe82198433c6763b4b96202faa9f0d1580e6
ikrama_shaikh <ikramashaikh939@gmail.com>
2022-09-27 17:53:25 +0530

    taskcommit

---
+++ src/Collection/Stacks.java
@@ -0,0 +1,19 @@
+package Collection;
+
+
+    import java.util.*;
+public class Stacks {
+        public static void main(String args[]){
+            Stack<String> stack = new Stack<String>();
+            stack.push("Ayush");
+            stack.push("Garvit");
+            stack.push("Amit");
+            stack.push("Ashish");
+            stack.push("Garima");
+            Iterator<String> itr=stack.iterator();
+            while(itr.hasNext()){
+                System.out.println(itr.next());
+            }
+        }
+    }
+
